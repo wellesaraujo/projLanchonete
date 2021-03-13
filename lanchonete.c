bb2f45775c607ccf2e64a870a878548984e38b1c
Wellington Araujo <welles.araujo@hotmail.com>
2021-03-13 20:04:43 -0300

    Imprimir saida em txt e ajustes de formatacao

--- lanchonete.c
+++ lanchonete.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
  typedef struct produto {
         char nome[20];
         float preco;
         int id;
-        produto* prox;
+        struct produto* prox;
     } produto ;
 
     typedef struct {
+        char categoria[20];
         produto* inicio;
     } Lista;
 
-    Lista* criaLista(){
+    Lista* criaLista(char categoria[]){
         Lista* nova = (Lista*) malloc (sizeof(Lista));
+        strcpy(nova->categoria, categoria);
         nova -> inicio = NULL;
         return nova;
     }
 
     Lista* Carrinho;
-  
+
     void inserirItem (Lista * L, char nomeItem[], float precoItem, int idItem){
         produto* novo = (produto*) malloc (sizeof(produto));
         strcpy(novo->nome, nomeItem);
@@ -38,13 +41,13 @@
             }
             aux->prox=novo;
         }
-        return;       
+        return;
     }
 
     produto* getProdutobyID(Lista* L, int id){
         produto* aux = L->inicio;
         while (aux!=NULL){
-            if (aux->id == id) 
+            if (aux->id == id)
                 return aux;
             aux = aux -> prox;
         }
@@ -68,15 +71,16 @@
             }
             aux->prox=itemComprado;
         }
-        return; 
+        return;
     }
 
     void exibirLista (Lista* L){
-        
+
         int op;
         int tamanhoLista;
 
         do{
+            printf ("\t   ## %s ##\n", L->categoria);
             produto* aux;
             aux=L->inicio;
             tamanhoLista=0;
@@ -87,58 +91,82 @@
                 tamanhoLista++;
             }
             free(aux);
-            
-            printf("\tPressione 0 para Voltar\n");
+            printf("\n\tPressione 0 para Voltar\n");
             scanf("%d", &op);
-            system("clear");
+            //system("clear");
             if(op>0 && op <=tamanhoLista){
                 addNoCarrinho(getProdutobyID(L, op));
-                printf ("\t%s adicionado ao carrinho!\n", getProdutobyID(L, op)->nome);
+                printf ("\n\t%s adicionado ao carrinho!\n", getProdutobyID(L, op)->nome);
+                sleep(1.5);
+                system("clear");
             }
             else if (op!=0)
                 printf("\tOpção inválida, tente novamente...\n");
-            
+
         } while (op!=0);
+    system("clear");
     return;
+    }
+
+    void imprimirRecibo (float total){
+        produto* aux = Carrinho->inicio;
+        FILE *nf;
+        nf = fopen("nf.txt","w");
+        fprintf(nf,"\t## RESUMO DO PEDIDO ##\n\n");
+        while (aux!=NULL){
+            fprintf(nf, "\t%s - R$%.2f \n", aux->nome, aux->preco);
+            aux = aux -> prox;
+        }
+        fprintf(nf, "\n\t## TOTAL = R$%.2f ##\n", total);
+        free(aux);
+        fclose(nf);
+        return;
     }
 
     void exibirCarrinho (){
         produto* aux = Carrinho->inicio;
         float total=0;
-        printf("\t   ##CARRINHO##\n");
+        printf("\t   ## CARRINHO ##\n\n");
         while (aux!=NULL){
             printf("\t%s - R$%.2f \n", aux->nome, aux->preco);
             total+=aux->preco;
             aux = aux -> prox;
         }
-        printf("\tTOTAL = R$%.2f\n", total);
+        printf("\n\tTOTAL = R$%.2f\n", total);
         free(aux);
-        printf("\tPressione 1 para Finalizar a Compra\n");
-        printf("\tPressione 0 para Voltar");
-        int op;
+
+        printf("\n\tPressione 1 para Finalizar a Compra\n");
+        printf("\tPressione 0 para Adicionar items\n");
+        int op=0;
         do{
             scanf("%d", &op);
             if(op==1){
-                //SALVAR EM TXT E FINALIZAR APP
-                printf("\tSALVAR LISTA EM TXT E FINALIZAR...\n");
+                printf("\tIMPRIMINDO PEDIDO, AGUARDE...\n\n");
+                sleep(1);
+                imprimirRecibo(total);
+                system("clear");
+                free(Carrinho);
+                printf("\tObrigadx, volte sempre!\n");
+                exit(0);
                 break;
             } else if (op!=0){
                 printf("\tOpção inválida, tente novamente...\n");
             }
         } while(op!=0);
+        system("clear");
         return;
     }
-    
+
     int main(){
 
-        char nome[20]; 
+        char nome[20];
         int id;
         float preco;
-        Lista* Lanches = criaLista();
-        Lista* Bebidas = criaLista();
-        Lista* Sobremesas = criaLista();
-        Carrinho = criaLista();
-        
+        Lista* Lanches = criaLista("LANCHES");
+        Lista* Bebidas = criaLista("BEBIDAS");
+        Lista* Sobremesas = criaLista("SOBREMESAS");
+        Carrinho = criaLista("CARRINHO");
+
         //Ler Arquivo Lanches
         FILE *lanchesTXT;
         lanchesTXT = fopen("InputFiles/lanches.txt", "r");
@@ -146,18 +174,18 @@
             do{
                 fscanf(lanchesTXT, "%d %s %f",&id, nome, &preco);
                 inserirItem(Lanches, nome, preco, id);
-            } while (!feof(lanchesTXT));     
+            } while (!feof(lanchesTXT));
         }
         fclose(lanchesTXT);
 
         //Ler Arquivo Bebidas
         FILE* bebidasTXT;
-        bebidasTXT = fopen("InputFiles/bebidas.txt", "r");  
+        bebidasTXT = fopen("InputFiles/bebidas.txt", "r");
         if (bebidasTXT){
             do{
                 fscanf(bebidasTXT, "%d %s %f",&id, nome, &preco);
                 inserirItem(Bebidas, nome, preco, id);
-            } while (!feof(bebidasTXT));     
+            } while (!feof(bebidasTXT));
         }
         fclose(bebidasTXT);
 
@@ -168,7 +196,7 @@
             do{
                 fscanf(sobremesasTXT, "%d %s %f",&id, nome, &preco);
                 inserirItem(Sobremesas, nome, preco, id);
-            } while (!feof(sobremesasTXT));     
+            } while (!feof(sobremesasTXT));
         }
         fclose(sobremesasTXT);
 
@@ -179,34 +207,34 @@
             printf ("O QUE DESEJA?\n");
             printf ("1 - Lanches\n2 - Bebidas\n3 - Sobremesas\n4 - Exibir Carrinho\nPressione 0 para Sair\n");
             scanf("%d", &op);
-                
+
             switch (op){
                 case (0) :
                     system("clear");
-                    printf("Obrigado, volte sempre!");
+                    printf("Obrigadx, volte sempre!");
                     free(Carrinho);
+                    free(Lanches);
+                    free(Bebidas);
+                    free(Sobremesas);
                     break;
-                case (1) : 
+                case (1) :
                     system("clear");
-                    printf ("\tESCOLHA UM LANCHE:\n");
                     exibirLista(Lanches);
                     break;
-                case (2) : 
+                case (2) :
                     system("clear");
-                    printf ("\tESCOLHA UMA BEBIDA:\n");
                     exibirLista(Bebidas);
                     break;
-                case (3) : 
+                case (3) :
                     system("clear");
-                    printf ("\tESCOLHA UMA SOBREMESA:\n");
                     exibirLista(Sobremesas);
                     break;
-                case (4) : 
+                case (4) :
                     system("clear");
                     exibirCarrinho();
                     break;
             }
         } while(op!=0);
-    
+
         return 0;
     }
